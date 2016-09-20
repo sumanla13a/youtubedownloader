55d217ef895283bc68f63d5a13e38644245a00ad
sumanla13a <lamasuman2@gmail.com>
2016-09-19 21:28:54 -0500

    fixes mp3 download

---
+++ Controller.js
@@ -0,0 +1,71 @@
+"use strict";
+var root    = process.cwd();
+var path    = require('path');
+var exec = require('child_process').exec;
+
+var Controller = {
+	download: function (req, res) {
+	    var pix = req.payload.pix;
+	    var youlink = req.payload.youlink;
+	    var command = ['youtube-dl', youlink];
+
+	    var quality;
+	    if('audio' === pix) {
+	     command = command.concat(['-o', path.join(root + '"/tmp/tmpAudio/%(title)s.%(ext)s"'), '--extract-audio', '--audio-format',  'mp3']);
+	    }
+	    else {
+	      command = command.concat(['-o', path.join(root + '"/tmp/tmpVideo/%(title)s.%(ext)s"')]);
+	      switch(pix) {
+	        case 240:
+	          quality= 133;
+	          break;
+	        case 360:
+	          quality= 134;
+	          break;
+	        case 480:
+	          quality= 135;
+	          break;
+	        case 720:
+	          quality= 136;
+	          break;
+	        case 1080:
+	          quality= 137;
+	          break;
+	        case 'best':
+	          quality= 22;
+	          break;
+	        default:
+	          quality=134;
+	      }
+	      command = command.concat(['-f', quality]);
+	    }
+	      
+	    function puts(error, er) {
+	      console.log(er);
+	      console.log(typeof(er)); 
+	      if(error) {
+	        console.log(error);
+	        return res(new Error('Sorry couldn\'t process your video'));
+	      }
+	      return res('Check your Videos directory after a while. It should be downloaded there');
+	    }
+	    exec(command.join(" "), puts);
+	},
+
+	getQuality: function(req, res) {
+		var youlink = req.payload.youlink;
+		var command = 'youtube-dl -F ' + youlink;
+		exec(command, function(err, res, pon) {
+			console.log('fuck');
+			console.log(err);
+			console.log('err done');
+			console.log(res);
+			console.log('std res');
+			console.log(pon);
+			console.log('stdError');
+
+		});
+	}
+};
+
+module.exports = Controller;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
 var Hapi = require('hapi');
 
-var server = new Hapi.Server();
+var server  = new Hapi.Server();
+var root    = process.cwd();
+var path    = require('path');
+var controller = require(path.join(root, 'Controller.js'));
 server.connection({port: 3000});
 
 server.route({
@@ -10,59 +13,14 @@ server.route({
   handler: function(req, res) {
     res('hello world');
   }
-})
+});
 
 server.route({
   method: 'POST',
   path: '/postLink',
-  handler: function (req, res) {
-    var pix = req.payload.pix;
-    var youlink = req.payload.youlink;
-    var vidtype = req.payload.vidtype;
-    var command = 'youtube-dl -o \"~/%(title)s.%(ext)s\" ';
-    if('video' === vidtype) {
-      command = command + youlink + ' '
-    }
-    var quality;
-    if(pix) {
-      switch(pix) {
-        case 'audio':
-          quality= 140;
-          break;
-        case 240:
-          quality= 133;
-          break;
-        case 360:
-          quality= 134;
-          break;
-        case 480:
-          quality= 135;
-          break;
-        case 720:
-          quality= 136;
-          break;
-        case 1080:
-          quality= 137;
-          break;
-        case 'best':
-          quality= 22;
-          break;
-        default:
-          quality=22;
-      }
-
-      command = command + '-f ' + quality;
-    }
-
-    console.log(command)
-    var sys = require('sys')
-    var exec = require('child_process').exec;
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec(command, puts);
-    // res('hahha')
-  }
-})
+  handler: controller.download
+});
 
 server.start(function() {
-  console.log('Your server has started at ', server.info.uri)
-})
+  console.log('Your server has started at ', server.info.uri);
+});
